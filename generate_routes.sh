8f74ebf63dee41320ddadc3a05ba522cfb9e0f33
Christian Giese <christian@rtbrick.com>
2024-04-24 13:18:58 +0000

    add comment to route script

--- generate_routes.sh
+++ generate_routes.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# NOTE: Replace eth0 with the actual TX interface, but remain the leading ":10", which refers to the VLAN
+# defines in the blaster.json confiuration. 
 bgpupdate -f rx1.bgp --local-pref 100 -n 192.0.2.194 -N 1 -p 11.0.0.0/24 -P 900000 --end-of-rib \
     -s streams.json \
     --stream-direction downstream \
